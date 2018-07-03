8073e5499037181a22247692df358372f03b952a
botoman <alessio.bottosso@gmail.com>
2018-07-03 15:59:56 +0200

    Max and min values.

--- js/main.js
+++ js/main.js
@@ -9,6 +9,10 @@ var Main = function(game)
     var motionGx;
     var motionGy;
     var motionGz;
+
+    var MX, MY, MZ;
+    var mx, my, mz;
+    var maxX, minX, maxY, minY, maxZ, minZ;
 };
 
 Main.prototype = 
@@ -19,6 +23,13 @@ Main.prototype =
 
         gn = new GyroNorm();
 
+        MX = 0;
+        MY = 0;
+        MZ = 0;
+        mx = 0;
+        my = 0;
+        mz = 0;
+
         availableText = game.add.text(16, 16, 'Accelerometer', { fontSize: '32px', fill: '#ffff' });
 
         motionX = game.add.text(16, 48, 'Motion x: ', { fontSize: '32px', fill: '#ffff' });
@@ -28,6 +39,13 @@ Main.prototype =
         motionGx = game.add.text(16, 173,  'Motion x + gravity: ', { fontSize: '32px', fill: '#ffff' });
         motionGy = game.add.text(16, 205, 'Motion y + gravity: ', { fontSize: '32px', fill: '#ffff' });
         motionGz = game.add.text(16, 237, 'Motion z + gravity: ', { fontSize: '32px', fill: '#ffff' });
+
+        maxX = game.add.text(24, 300, 'Max x: ', { fontSize: '40px', fill: '#ffff' });
+        maxY = game.add.text(24, 350, 'Max y: ', { fontSize: '40px', fill: '#ffff' });
+        maxZ = game.add.text(24, 400, 'Max Z: ', { fontSize: '40px', fill: '#ffff' });
+        minX = game.add.text(24, 500, 'Min x: ', { fontSize: '40px', fill: '#ffff' });
+        minY = game.add.text(24, 550, 'Min y: ', { fontSize: '40px', fill: '#ffff' });
+        minZ = game.add.text(24, 600, 'Min Z: ', { fontSize: '40px', fill: '#ffff' });
 
         gn.init().then(function() 
         {
@@ -64,4 +82,37 @@ function gnCallback (data)
     motionGx.text = 'Motion x + gravity: ' + data.dm.gx;
     motionGy.text = 'Motion y + gravity: ' + data.dm.gy;
     motionGz.text = 'Motion z + gravity: ' + data.dm.gz;
+
+    if (data.dm.x > MX)
+    {
+        MX = data.dm.x;
+    }
+    else if (data.dm.x < mx)
+    {
+        mx = data.dm.x;
+    }
+    if (data.dm.y > MY)
+    {
+        MY = data.dm.y;
+    }
+    else if (data.dm.y < my)
+    {
+        my = data.dm.y;
+    }
+    if (data.dm.z > MZ)
+    {
+        MZ = data.dm.z;
+    }
+    else if (data.dm.z < mz)
+    {
+        mz = data.dm.z;
+    }
+
+    maxX.text = 'Max x: ' + MX;
+    maxY.text = 'Max y: ' + MY;
+    maxZ.text = 'Max z: ' + MZ;
+
+    minX.text = 'Min x: ' + mx;
+    minY.text = 'Min y: ' + my;
+    minZ.text = 'Min z: ' + mz;
 }
